9dc0afd6670aeb4dc6fc43bbf5b400f4d2b624de
dlouis91 <louisdesj91@gmail.com>
2024-12-16 12:01:56 +0100

    tests unitaires

--- Test_unitaires_Parking.py
+++ Test_unitaires_Parking.py
@@ -6,19 +6,19 @@ class TestParking(unittest.TestCase):
         self.parking = Parking(2, 2)  # Parking avec 2 étages et 2 places par étage
 
     def test_initialisation(self):
-        self.assertEqual(self.parking.places_libres, 4)
+        self.assertEqual(self.parking.places_libres, 4) # vérification 4 places libres a l'init
 
 
     def test_ajouter_voiture(self):
         result = self.parking.ajouter_voiture()
         self.assertEqual(result, (1, 1))
-        self.assertEqual(self.parking.places_libres, 3)
+        self.assertEqual(self.parking.places_libres, 3) # verif si on ajoute 1 voiture, on a plus que 3 places
 
     def test_retirer_voiture(self):
         self.parking.ajouter_voiture(1, 1)
         result = self.parking.retirer_voiture(1, 1)
         self.assertTrue(result)
-        self.assertEqual(self.parking.places_libres, 4)
+        self.assertEqual(self.parking.places_libres, 4) # verif si on ajoute et retire 1 voiture, on a 4 places
 
 if __name__ == "__main__":
     unittest.main()
